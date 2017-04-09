42acde9436d5c2d72805ceaefa38ab721e83330e
MP Mac <mpmac@mps-macbook-pro.lan>
2017-04-09 14:29:22 +0300

    Partical system

--- Game3D/GameViewController.swift
+++ Game3D/GameViewController.swift
@@ -11,115 +11,98 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
+    
+   
+    var scnView: SCNView!
+    var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+    var sphere: SCNNode!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        setupView()
+        setupScene()
+        createSphere()
+        setupCamera()
+        setupLight()
+ //       createTrail()
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
         
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+    }
+    
+    func setupView(){
+        self.view.backgroundColor = UIColor.white
+        scnView = self.view as! SCNView
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = .omni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
-        
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = .ambient
-        ambientLightNode.light!.color = UIColor.darkGray
-        scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // set the scene to the view
-        scnView.scene = scene
-        
-        // allows the user to manipulate the camera
+    }
+   
+    func setupScene(){
+        scnScene = SCNScene()
+        scnView.scene = scnScene
         scnView.allowsCameraControl = true
+       // scnView.autoenablesDefaultLighting = true
         
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
-        
-        // configure the view
-        scnView.backgroundColor = UIColor.black
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
-    }
-    
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
-        
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result: AnyObject = hitResults[0]
-            
-            // get its material
-            let material = result.node!.geometry!.firstMaterial!
-            
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
-            
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
-            
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
-        }
-    }
-    
-    override var shouldAutorotate: Bool {
-        return true
-    }
-    
-    override var prefersStatusBarHidden: Bool {
-        return true
-    }
-    
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
     }
 
+    func setupCamera(){
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.camera?.usesOrthographicProjection = true
+        cameraNode.camera?.orthographicScale = 8
+        cameraNode.position = SCNVector3Make(20, 20, 20)
+        cameraNode.eulerAngles = SCNVector3Make(-45, 45, 0)
+        let constraint = SCNLookAtConstraint(target: sphere)
+        constraint.isGimbalLockEnabled = true
+        cameraNode.constraints = [constraint]
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    func createSphere() {
+        sphere = SCNNode()
+        
+        let sphereGeometry = SCNSphere(radius: 1.5)
+        sphere.geometry = sphereGeometry
+        
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIColor.blue
+        sphereGeometry.materials = [sphereMaterial]
+        sphere.position = SCNVector3Make(0, 0, 0)
+        scnScene.rootNode.addChildNode(sphere)
+    }
+
+    func setupLight() {
+        
+        // Lightspot from top with shadows
+        let light = SCNNode ()
+        light.light = SCNLight ()
+        light.light?.type = SCNLight.LightType.spot
+        light.light?.color = UIColor.white
+        light.position = SCNVector3(0, 20, 0)
+        light.castsShadow = false
+        light.light?.shadowRadius = 2
+        light.light?.shadowColor = UIColor.black
+        scnScene.rootNode.addChildNode(light)
+        
+        let light2 = SCNNode ()
+        light2.light = SCNLight ()
+        light2.light?.type = SCNLight.LightType.directional
+        light2.light?.color = UIColor.white
+        light2.position = SCNVector3Make(-45, 45, 0)
+        scnScene.rootNode.addChildNode(light2)
+    }
+    
+    //Kame problema del funkcijos createTrail, savybes color?, bei kaip panaudoti sukurta sfera is funkcijos createSphere?
+    
+//    func createTrail(color: UIColor: , geometry: SCNGeometry) -> SCNParticleSystem {
+//        
+//        let trail = SCNParticleSystem(named: "ParticalSpheres.scnp", inDirectory: nil)!
+//        
+//        trail.particleColor = color
+//        
+//        trail.emitterShape = geometry
+//        
+//        return trail
+//    }
 }
